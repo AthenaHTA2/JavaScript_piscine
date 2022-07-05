c2cfd6a3539b5af90d865ffe00d53c46803cfb10
AthenaHTA2 <sedmakh2@gmail.com>
2022-07-05 21:32:05 +0100

    js ex 14

---
+++ abs.js
@@ -0,0 +1,17 @@
+function abs(n){
+    if (n < 0){
+        return n * -1;
+    }
+    else {
+        return n;
+    }
+};
+
+function isPositive(n){
+    if (n - n === 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+};
