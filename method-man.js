a0b59c2676febca3b085fe2b24ac0dfff5fa2f29
AthenaHTA2 <sedmakh2@gmail.com>
2022-07-05 21:01:56 +0100

    js ex 13

---
+++ method-man.js
@@ -0,0 +1,19 @@
+function words(a){
+    return a.split(" ");
+};
+//const words = (i) => i.split(" ") example of shorter code
+
+function sentence(b){
+    return b.join();
+};
+
+function yell(c){
+    return c.toUpperCase();
+};
+
+function whisper(d){
+    return "*"+d.toLowerCase()+"*";
+};
+
+const capitalize = (e) => e.charAt(0).toUpperCase() + e.slice(1).toLower()
+
