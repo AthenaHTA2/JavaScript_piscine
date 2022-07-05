1f8021ac574512061854e00fd4191b359427c444
AthenaHTA2 <sedmakh2@gmail.com>
2022-07-05 13:48:19 +0100

    JS ex 4

---
+++ mutability.js
@@ -0,0 +1,17 @@
+var person = {
+    'age': 100,
+    'country': "IT"
+};
+
+var samePerson = person;
+
+const clone1 = person;
+
+const clone2 = person;
+
+person[age] = person[age] + 1;
+person[country] = "FR";
+
+
+
+
