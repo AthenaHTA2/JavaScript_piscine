9626240a38f8bce9ad8f5a549bf6ff0bf176215c
AthenaHTA2 <sedmakh2@gmail.com>
2022-07-08 17:23:06 +0100

    jsQ2 ex5

--- keep-cut.js
+++ keep-cut.js
@@ -6,4 +6,52 @@ function cutFirst(str) {
   return res;
 }
 
-console.log(cutFirst("01Founders"));
+function cutLast(str) {
+  let res = "";
+  for (let i = 0; i < str.length - 2; i++) {
+    res += str[i];
+  }
+  return res;
+}
+
+function cutFirstLast(str) {
+  let res = "";
+  for (let i = 2; i < str.length - 2; i++) {
+    res += str[i];
+  }
+  return res;
+}
+
+function keepFirst(str) {
+  let res = "";
+  if (str.length < 2) {
+    return str;
+  }
+  for (let i = 0; i < 2; i++) {
+    res += str[i];
+  }
+  return res;
+}
+
+function keepLast(str) {
+  let res = "";
+  if (str.length < 2) {
+    return str;
+  }
+  for (let i = str.length - 2; i <= str.length - 1; i++) {
+    res += str[i];
+  }
+  return res;
+}
+
+function keepFirstLast(str) {
+  let res = "";
+  if (str.length < 4) {
+    return str;
+  }
+  res = keepFirst(str) + keepLast(str);
+
+  return res;
+}
+
+console.log(keepFirstLast("01Founders"));
