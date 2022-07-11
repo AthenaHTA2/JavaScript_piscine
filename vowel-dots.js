d3757a9b915b21404e048937d15912ac2e6700db
AthenaHTA2 <sedmakh2@gmail.com>
2022-07-11 10:24:56 +0100

    jsQ3 ex3

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,3 +1,10 @@
+/*Create a function named vowelDots that receives a string. Your function should return a new string with a . after every vowel.
+
+Your RegEx should be stored in a variable named vowels.
+
+    a, e, i, o and u are considered as vowels here.
+*/
+
 function vowelDots(str) {
   if (str.length == 0) return "";
 
