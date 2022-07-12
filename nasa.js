f46ef36a917b91641a97ca8acbee632bad95c2e9
AthenaHTA2 <sedmakh2@gmail.com>
2022-07-12 20:47:11 +0100

    jsQ2 ex10

--- nasa.js
+++ nasa.js
@@ -1,3 +1,11 @@
+/*Instructions
+
+Create a function named nasa that takes a number N as an argument and returns a string with all numbers from 1 to N separated by spaces. There are three exceptions:
+
+    Convert numbers which are divisible by 3 to "NA".
+    Convert numbers which are divisible by 5 to "SA".
+    Convert numbers which are divisible by 3 and 5 to "NASA".*/
+
 function nasa(N) {
   let str = "";
   for (let i = 1; i <= N; i++) {
