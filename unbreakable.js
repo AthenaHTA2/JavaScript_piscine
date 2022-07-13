c6c7f4730a32dbdf995598985d50ac951d59b712
AthenaHTA2 <sedmakh2@gmail.com>
2022-07-13 19:47:04 +0100

    jsq2 ex8

--- unbreakable.js
+++ unbreakable.js
@@ -1,4 +1,58 @@
-/*Implement 2 functions:
+/*
+Split solution by Cassidy:
+function split(str, pat) {
+  let s = "";
+  let cut = "";
+  let answer = [];
+  if (str.length < 1) {
+    answer.push(str);
+    return answer;
+  }
+  if (pat.length == 0) {
+    for (let i = 0; i < str.length; i++) {
+      answer.push(str[i]);
+    }
+    return answer;
+  }
+  if (pat.length >= 1) {
+    for (let i = 0; i <= str.length; i++) {
+      if (str[i] != pat[0] && str[i] != undefined) {
+        s += str[i];
+      }
+      if (str[i] == pat[0] && pat.length <= 2) {
+        if (i == str.length) {
+        }
+        if (i != 0) {
+          answer.push(s);
+        }
+        s = "";
+      }
+    }
+    for (let i = 0; i <= s.length; i++) {
+      if (s[i] != pat[1] && s[i] != " ") {
+        cut += s[i];
+      }
+      if (s[i] == pat[1] || i == s.length - 1) {
+        if (s[i] != undefined) {
+          answer.push(cut);
+          cut = "";
+        }
+        if (i == s.length && str == "ee,ff,g,") {
+          answer.push("");
+          return answer;
+        } else if (i == s.length && str != "ee,ff,g,") {
+          return answer;
+        }
+      }
+    }
+    return answer;
+  }
+}
+
+******************* My Solution *****************************
+
+
+Implement 2 functions:
 
     split that works like String.split, but takes the string as its first argument.
 
