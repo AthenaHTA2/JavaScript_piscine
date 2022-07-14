3d74dc570e44aebe27586a03e5bcafd84ebbc805
AthenaHTA2 <sedmakh2@gmail.com>
2022-07-14 12:43:31 +0100

    jsQ2 ex13

--- triangle.js
+++ triangle.js
@@ -10,20 +10,35 @@ const times = (nb) => {
         result = result + n
     }
     return result
-}*/
+}
+Falsey values in JavaScript:
+1) false
+2) Zero of Number type: 0 and also -0, 0.0, and hex form 0x0 (thanks RBT)
+3) Zero of BigInt type: 0n and 0x0n (new in 2020, thanks GetMeARemoteJob)
+4) "", '' and `` - strings of length 0
+5) null
+6) undefined
+7) NaN
+8) document.all (in HTML browsers only)
+
+
+*/
 
 function triangle(str, nb) {
   let out = "";
   for (var i = 0; i < nb; i++) {
+    console.log("i: ", i);
     for (var j = 0; j <= i; j++) {
+      console.log("j: ", j);
       out += str;
     }
-
+    console.log(out);
     out += "\n";
   }
-  console.log(i);
-  console.log(j);
-  console.log(out);
+
+  // console.log(i);
+  // console.log(j);
+  //console.log(out);
 }
 
 console.log(triangle(triangle("a", 5)));
