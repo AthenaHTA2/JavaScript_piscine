28f3c5a9378ab12a92e0fb078c13f52cf6c7bca9
AthenaHTA2 <sedmakh2@gmail.com>
2022-07-24 21:19:19 +0100

    jsQ5 ex1

---
+++ using-map.js
@@ -0,0 +1,14 @@
+function citiesOnly(arrCities) {
+  var cities = arr1.map(function (city) {
+    return arr1.city;
+  });
+
+  return cities;
+}
+
+function upperCasingStates(arrStates) {
+  var states = arrStates.map(function (state) {
+    return arrStates[state].string.charAt(0).toUpperCase;
+  });
+  return states;
+}
