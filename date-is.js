a73f1d4e1424bc89ab8e7c218d682e4b9da0eb17
AthenaHTA2 <sedmakh2@gmail.com>
2022-08-26 19:36:47 +0100

    jsQ4 ex2

--- date-is.js
+++ date-is.js
@@ -6,47 +6,115 @@
     isFuture: accepts a Date, and returns true if the Date is valid, and is after than the present date.
     isPast: accepts a Date, and returns true if the Date is valid, and is before the present date.
 */
-
-/*
-Google: How check date is valid or invalid in JavaScript?
-Store the date object in a variable. If the date is valid then the getTime() will always be equal to itself. 
-If the date is Invalid then the getTime() will return NaN which is not equal to itself. 
-The isValid() function is used to check the getTime() method is equal to itself or not.
-*/
-
-function isValid(myDate) {
-  /*console.log(new Date());
-  console.log(Object.prototype.toString.call(myDate));
-  console.log("[object myDate]");
-  console.log(!isNaN(Date.parse(myDate)));
-  let test = Object.prototype.toString.call(myDate) instanceof Date;
-  if (
-    Object.prototype.toString.call(myDate) === "[object myDate]" &&
-    !isNaN(myDate)
-  ) {
+//working for all tests
+function isValid(d) {
+  //if input is a date
+  if (d instanceof Date === true) {
     return true;
+  } else if (typeof d === "number" && new Date(d) instanceof Date === true) {
+    //if input is #millliseconds
+    //turn #milliseconds into a date object and check if it is a valid date
+    if (new Date(d) instanceof Date === true) {
+      return true;
+    }
   }
-  console.log(Date.now());
-  console.log(Object.prototype.toString.call(myDate));
-  console.log(new Date());
-  //console.log(myDate.getTime());*/
-
-  if (typeof myDate === "string") {
-    return false;
-  }
-  if (myDate == Date.now()) {
-    return true;
-  }
-  return !isNaN(Date.parse(myDate));
+  return false;
 }
 
-console.log(isValid(Date.now()));
-//console.log(isValid("2013-01-01")); gives false
-//console.log(isValid(isValid)); gives false
-//console.log(isValid(Date.now()));gives false
-//console.log(isValid(new Date()));gives true
-//console.log(isValid(Date.now()));gives false
-//console.log(isValid(new Date("December 17, 1995 03:24:00")));gives true
-//console.log(isValid(new Date(new Date(1488370835081)))); gives true
-//console.log(isValid(new Date("1995-12-17T03:24:00"))); gives true
-//console.log(isValid(new Date("1995-12-17T03:24:00").getTime())); gives false
+function isAfter(dateOne, dateTwo) {
+  // console.log(new Date(819170640000))
+  // console.log(new Date(123123))
+  // console.log(new Date(526))
+  if (isValid(dateOne) && isValid(dateTwo)) {
+    if (
+      typeof dateOne === "number" &&
+      typeof dateTwo === "number" &&
+      dateOne - dateTwo > 0
+    ) {
+      return true;
+    } else {
+      let difference = dateOne.getTime() - dateTwo.getTime(); //turn each date in # of milliseconds and take the difference
+      if (difference > 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function isBefore(dateOne, dateTwo) {
+  if (isValid(dateOne) && isValid(dateTwo)) {
+    if (
+      typeof dateOne === "number" &&
+      typeof dateTwo === "number" &&
+      dateTwo - dateOne > 0
+    ) {
+      return true;
+    } else {
+      let difference = dateTwo.getTime() - dateOne.getTime();
+      if (difference > 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function isFuture(d) {
+  if (isValid(d)) {
+    let presentTime = new Date();
+    if (d.getTime() - presentTime.getTime() > 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isPast(d) {
+  if (isValid(d)) {
+    let presentTime = new Date();
+    if (presentTime.getTime() - d.getTime() > 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//test cases:
+//console.log(isValid(new Date(123123))); //gives true
+//console.log(isValid(new Date())); // returns a string date and gives true
+//console.log(isValid(Date.now())); //returns # of milliseconds and gives true
+//console.log(isValid(`'2013-01-01'`)); //gives false
+//console.log(isValid(isValid)); //gives false
+//console.log(isValid(`''`));//gives false
+//console.log(isValid(`NaN`));//gives false
+//console.log(isValid(Date.now())); //gives true
+//console.log(isValid(new Date("December 17, 1995 03:24:00")));//gives true
+//console.log(isValid(new Date(1488370835081))); //gives true
+//console.log(isValid(new Date("1995-12-17T03:24:00"))); //gives true
+//console.log(isValid(new Date("1995-12-17T03:24:00").getTime())); //turns the date into milliseconds. gives true
+
+//isAfter
+//console.log(isAfter(new Date("1992-01-01"), new Date("1992-01-02")));//return false
+//console.log(isAfter(new Date('2157-11-07'), new Date('2183-04-16')))//return false
+//console.log(isAfter(new Date(2321, 11, 21), new Date(Date.now())))//return true
+//console.log(isAfter(123123, 526)); //return true
+// // isBefore
+
+//console.log(isBefore(new Date(2321, 11, 21), new Date(Date.now())))
+console.log(isBefore(123123, 526)); //returns false not correct yet
+//console.log(isBefore(new Date('1992-01-01'), new Date('1992-01-02')))
+ //console.log(isBefore(new Date('2157-11-07'), new Date('2183-04-16')))
+// // isFuture
+
+//console.log(isFuture(new Date('1992-01-01')))
+//console.log(isFuture(new Date(Date.now() - 1)))
+// console.log(isFuture(new Date(2077, 11, 31)))
+//console.log(isFuture(new Date(Date.now() + 1)))
+// // isPast
+
+//console.log(isPast(new Date(Date.now() + 1)))//returns false
+//console.log(isPast(new Date(2077, 11, 31)))//returns false
+//console.log(isPast(new Date(1442, 11, 31)))//returns true
+//console.log(isPast(new Date(Date.now() - 1)))//returns true
