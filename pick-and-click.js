234687a73c3b2839a2340e825e014ee25c545a72
AthenaHTA2 <sedmakh2@gmail.com>
2022-09-02 18:08:34 +0100

    jsq5 ex12

--- pick-and-click.js
+++ pick-and-click.js
@@ -1,3 +1,27 @@
+/*Today, you're going to create your own color picker.
+
+Write the function pick which turns the screen into a hsl color picker. It will vary the hue and luminosity according to the position of the mouse.
+
+The background color of the body will change based on the position of the mouse on the screen.
+
+The X axis will vary the hue value between 0 and 360.
+The Y axis will vary the luminosity value between 0 and 100.
+You'll need to display these three values:
+
+The full hsl value in a div, which has the class hsl in the middle of the screen.
+The hue value in a div with the class hue and text in the top right corner of the screen.
+The luminosity value will be displayed in the bottom left corner of the screen, in a div with the class luminosity and text.
+When the mouse is clicked, the value of the hsl will need to be copied to the clipboard.
+
+Two SVG lines with ids axisX and axisY will need to follow the cursor, like really long cross hairs.
+
+the axisX attributes x1 and x2 need to be set to the X position of the cursor.
+the axisY attributes y1 and y2 need to be set to the Y position of the cursor.
+The formatting of a hsl value: hsl(45, 50%, 35%).
+
+Use Math.round() to round the values.
+*/
+
 export function pick(mouse) {
     if (mouse == undefined) {
         return
